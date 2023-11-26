1947332a25d37f08c36b342ef6ea1cf116e1db9b
monter <tmszczepanik@outlook.com>
2023-11-26 02:11:39 +0100

    Add CardView, extend Card class

--- bridge-app/src/common/deck/card.ts
+++ bridge-app/src/common/deck/card.ts
@@ -4,9 +4,11 @@ import {Rank} from './rank'
 class Card {
     suit: Suit
     rank: Rank
-    constructor(suit: Suit, rank: Rank) {
+    id: Number
+    constructor(suit: Suit, rank: Rank, id: Number) {
         this.suit = suit
         this.rank = rank
+        this.id = id
     }
 }
 
--- bridge-app/src/pages/Game.js
+++ bridge-app/src/pages/Game.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import {Dealer} from '../common/deck/dealer'
+import {CardView} from '../components/CardView'
+
+import '../style/Hand.css'
+import { Card } from '@mui/material';
+
+const dealer = new Dealer();
+const hands = dealer.deal();
 
 const Game = () => {
-  const dealer = new Dealer();
-  const hands = dealer.deal();
-  
   console.log("Deal")
   console.log(hands.north)
   console.log(hands.south)
   console.log(hands.west)
   console.log(hands.west)
   console.log("----");
+
+  const cardsSouth = hands.south.cards
+  const [updatedCardsSouth, updateCardsSouth] = React.useState(cardsSouth)
+  
+  const cardPlayedSouth = []
+  
+  function handleRemove(id) {
+    const newCardsSouth = updatedCardsSouth.filter((card) => card.id !== id);
+    updateCardsSouth(newCardsSouth)
+  }
+
   return (
-    <div>
-    <h2>Game will begin!</h2>
-    </div>
-  );
+    <ul className='Hand'>
+      {updatedCardsSouth.map((card) => (
+        <li onClick={() => handleRemove(card.id)}>
+        <CardView id={card.id} />
+        </li>))}
+    </ul>
+  )
 };
 
 export default Game;
