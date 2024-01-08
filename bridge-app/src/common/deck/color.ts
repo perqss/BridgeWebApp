1cd21566787434d597450c3cc8372932e4850ad0
HP <arkadiusz.bialy@onet.pl>
2024-01-08 07:15:47 +0100

    add value object design pattern

--- bridge-app/src/common/deck/color.ts
+++ bridge-app/src/common/deck/color.ts
@@ -1,6 +1,12 @@
+import { lightBlue } from "../utils"
+
 enum Color {
     black = 'black',
     red = 'red',
+    orange = 'orange',
+    grey = 'grey',
+    white = 'white',
+    blue = '#4287f5',
 }
 
 export {Color}
